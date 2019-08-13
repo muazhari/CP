3a2ee93b4d624ba43abd63aef656c194102fd964
Muhammad Kharisma Azhari <39398937+muazhari@users.noreply.github.com>
2019-08-13 13:30:10 +0700

    Update

---
+++ HackerRank/30 Days of Code/Day1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+	int i = 4;
+	double d = 4.0;
+	char s[] = "HackerRank ";
+	char si[] = "aaa";
+
+	scanf("%[^\n]s",si);
+
+	printf("%s",si);
+
+
+	return 0;
+}
