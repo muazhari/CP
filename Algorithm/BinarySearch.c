94e2772b95ad05f6b109d79d9cd639d923f39409
Muhammad Kharisma Azhari <39398937+muazhari@users.noreply.github.com>
2019-08-16 15:55:40 +0700

    Update

--- Algorithm/BinarySearch.c
+++ Algorithm/BinarySearch.c
@@ -7,7 +7,7 @@
 
 struct list
 {
-    int *array;
+    long int *array;
     size_t length;
 } typedef list;
 
@@ -49,12 +49,11 @@ list *slice(list *fromList, int i, int n)
     n = i + n > fromList->length ? fromList->length - i : n;
 
     list *x = newList(n);
-    // printf("fromList len: %d result len: %d\n", fromList->length, n);
     int count = 0;
+
     while (count < n)
     {
         x->array[count] = *(fromList->array + i);
-        // printf("%d\n", x->array[count]);
         count += 1;
         i += 1;
     }
@@ -87,37 +86,74 @@ int binarySearch(list *x, int num)
     }
 }
 
+int binarySearch2(list *x, int l, int r, int num)
+{
+
+    long int mid = (l + r) / 2;
+    // printf("mid: %d, l: %d, r: %d\n", x->array[mid], l, r);
+    list *sliced = slice(x, l, r - l);
+    printl(sliced);
+
+    if (num == x->array[mid])
+    {
+        return 1;
+    }
+    else if (l == r || r - l == 1 && num != x->array[mid])
+    {
+        return 0;
+    }
+    else if (num > x->array[mid])
+    {
+        return binarySearch2(x, mid, r, num);
+    }
+    else if (num < x->array[mid])
+    {
+        return binarySearch2(x, l, mid, num);
+    }
+}
+
 void test_slice(int id, list *t, int expect)
 {
     assert(t->length == expect);
     freeList(t);
+    printf("==================================\n");
     printf("test_slice_%d OK\n", id);
+    printf("==================================\n");
 }
 
 void test_binsearch(int id, int t, int expect)
 {
     assert(t == expect);
+    printf("==================================\n");
     printf("test_binsearch_%d OK\n", id);
+    printf("==================================\n");
 }
 
-void test_unit(list *nums)
+void test_unit(list *x)
 {
-    test_slice(1, slice(nums, 3, 16), 12);
-    test_slice(2, slice(nums, 0, 0), 0);
-    test_slice(3, slice(nums, -1, 99999), 0);
-    test_slice(4, slice(nums, 0, 99999), 15);
-    test_slice(5, slice(nums, 0, -1), 15);
+    // test_slice(1, slice(x, 3, 16), 12);
+    // test_slice(2, slice(x, 0, 0), 0);
+    // test_slice(3, slice(x, -1, 99999), 0);
+    // test_slice(4, slice(x, 0, 99999), 15);
+    // test_slice(5, slice(x, 0, -1), 15);
 
-    test_binsearch(1, binarySearch(nums, 11), 1);
-    test_binsearch(2, binarySearch(nums, 99), 1);
-    test_binsearch(3, binarySearch(nums, 1237), 1);
-    test_binsearch(4, binarySearch(nums, -1), 0);
-    test_binsearch(5, binarySearch(nums, 99999), 0);
+    test_binsearch(1, binarySearch(x, 11), 1);
+    test_binsearch(2, binarySearch(x, 99), 1);
+    test_binsearch(3, binarySearch(x, 1237), 1);
+    test_binsearch(4, binarySearch(x, -1), 0);
+    test_binsearch(5, binarySearch(x, 99999), 0);
+
+    test_binsearch(1, binarySearch2(x, 0, x->length, 11), 1);
+    test_binsearch(2, binarySearch2(x, 0, x->length, 99), 1);
+    test_binsearch(3, binarySearch2(x, 0, x->length, 1237), 1);
+    test_binsearch(4, binarySearch2(x, 0, x->length, -1), 0);
+    test_binsearch(5, binarySearch2(x, 0, x->length, 99999), 0);
 }
 
 int main()
 {
-    int raw_array[] = {11, 24, 30, 43, 51, 61, 73, 86, 99, 134, 353, 579, 781, 959, 1237};
+    int raw_array[] = {11, 24, 30, 43, 99, 1237};
+    char raw_array2[] = "halo";
 
     // declare array as list
     list *nums = newList(arrayLength(raw_array));
@@ -126,6 +162,6 @@ int main()
 
     test_unit(nums);
 
-    int find = binarySearch(nums, 579);
+    int find = binarySearch(nums, 24);
     printf("%d", find);
 }
